7cdd440213b1592b093659d6a7be3c8e510c2811
Dan Compton <dan@opsee.com>
2016-05-27 16:01:50 -0400

    Update basic and protobuf

---
+++ vendor/github.com/opsee/basic/schema/aws/copy.go
@@ -0,0 +1,112 @@
+package aws
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	opsee_types "github.com/opsee/protobuf/opseeproto/types"
+	"io"
+	"reflect"
+	"time"
+)
+
+// Copy deeply copies a src structure to dst. Useful for copying request and
+// response structures.
+//
+// Can copy between structs of different type, but will only copy fields which
+// are assignable, and exist in both structs. Fields which are not assignable,
+// or do not exist in both structs are ignored.
+func CopyInto(dst, src interface{}) {
+	dstval := reflect.ValueOf(dst)
+	if !dstval.IsValid() {
+		panic("Copy dst cannot be nil")
+	}
+
+	rcopy(dstval, reflect.ValueOf(src), true)
+}
+
+// rcopy performs a recursive copy of values from the source to destination.
+//
+// root is used to skip certain aspects of the copy which are not valid
+// for the root node of a object.
+func rcopy(dst, src reflect.Value, root bool) {
+	if !src.IsValid() {
+		return
+	}
+
+	switch src.Kind() {
+	case reflect.Ptr:
+		if _, ok := src.Interface().(io.Reader); ok {
+			if dst.Kind() == reflect.Ptr && dst.Elem().CanSet() {
+				dst.Elem().Set(src)
+			} else if dst.CanSet() {
+				dst.Set(src)
+			}
+		} else if tt, ok := src.Interface().(*time.Time); ok {
+			if tt != nil && dst.CanSet() {
+				timestamp := &opsee_types.Timestamp{}
+				timestamp.Scan(*tt)
+				dst.Set(reflect.ValueOf(timestamp))
+			}
+		} else if tt, ok := src.Interface().(*opsee_types.Timestamp); ok {
+			if tt != nil && dst.CanSet() {
+				t, _ := tt.Value()
+				dst.Set(reflect.ValueOf(aws.Time(t.(time.Time))))
+			}
+		} else if dst.Kind() != reflect.Ptr {
+			if dst.CanSet() && !src.IsNil() {
+				dst.Set(reflect.Indirect(src))
+			}
+		} else {
+			de := dst.Type().Elem()
+			if dst.CanSet() && !src.IsNil() {
+				dst.Set(reflect.New(de))
+			}
+			if src.Elem().IsValid() {
+				// Keep the current root state since the depth hasn't changed
+				rcopy(dst.Elem(), src.Elem(), root)
+			}
+		}
+	case reflect.Struct:
+		t := dst.Type()
+		for i := 0; i < t.NumField(); i++ {
+			name := t.Field(i).Name
+			srcVal := src.FieldByName(name)
+			dstVal := dst.FieldByName(name)
+			if srcVal.IsValid() && dstVal.CanSet() {
+				rcopy(dstVal, srcVal, false)
+			}
+		}
+	case reflect.Slice:
+		if src.IsNil() {
+			break
+		}
+
+		s := reflect.MakeSlice(dst.Type(), src.Len(), src.Cap())
+		dst.Set(s)
+		for i := 0; i < src.Len(); i++ {
+			rcopy(dst.Index(i), src.Index(i), false)
+		}
+	case reflect.Map:
+		if src.IsNil() {
+			break
+		}
+
+		s := reflect.MakeMap(dst.Type())
+		dst.Set(s)
+		for _, k := range src.MapKeys() {
+			v := src.MapIndex(k)
+			v2 := reflect.New(v.Type()).Elem()
+			rcopy(v2, v, false)
+			dst.SetMapIndex(k, v2)
+		}
+	default:
+		if dst.Kind() == reflect.Ptr && src.CanAddr() {
+			src = src.Addr()
+		}
+
+		if src.Type().AssignableTo(dst.Type()) {
+			dst.Set(src)
+		} else {
+			dst.Set(src.Convert(dst.Type()))
+		}
+	}
+}
