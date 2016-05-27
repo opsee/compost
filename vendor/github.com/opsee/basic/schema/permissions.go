621d9ee199c518efb0f19272e2433f9006048c32
Dan Compton <dan@opsee.com>
2016-05-27 15:44:32 -0400

    Bump basic again

--- vendor/github.com/opsee/basic/schema/permissions.go
+++ vendor/github.com/opsee/basic/schema/permissions.go
@@ -1,29 +1,10 @@
 package schema
 
-import (
-	"sync"
-
-	opsee_types "github.com/opsee/protobuf/opseeproto/types"
-)
+import opsee_types "github.com/opsee/protobuf/opseeproto/types"
 
 func init() {
-	opsee_types.PermissionsRegistry.Register("user", &opsee_types.PermissionsBitmap{
-		map[int]string{
-			0: "admin",
-			1: "edit",
-			2: "billing",
-		},
-		sync.RWMutex{},
-	})
-
-	opsee_types.PermissionsRegistry.Register("team", &opsee_types.PermissionsBitmap{
-		map[int]string{
-			0: "multi-user",
-			1: "multibastion",
-			2: "on-site support",
-		},
-		sync.RWMutex{},
-	})
+	opsee_types.PermissionsRegistry.Register("user", opsee_types.NewPermissionsBitmap("admin", "edit", "billing"))
+	opsee_types.PermissionsRegistry.Register("team", opsee_types.NewPermissionsBitmap("multi_user", "multi_bastion", "on_site_support"))
 }
 
 // Wrapper for check if requesting user can modify target
